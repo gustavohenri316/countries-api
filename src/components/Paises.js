7b62e7812303b509422982772371442384789789
Gustavo Henrique <64738844+gustavohenri316@users.noreply.github.com>
2021-09-12 16:23:12 -0300

    Update Paises.js

--- src/components/Paises.js
+++ src/components/Paises.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import Filter from "./Filter"
+import Filter from "./Filtro"
 
 const url = "https://restcountries.eu/rest/v2/all"
 
